e4d919427ef860240b9169f4a9eca00448d3fcfd
Ayaka Miyake <ayakamiyake7@gmail.com>
2024-08-20 12:13:12 +0900

    Learn react router

--- src/App.js
+++ src/App.js
@@ -1,30 +1,36 @@
+import { BrowserRouter, Link } from "react-router-dom";
+
 import "./App.css";
-import { useState, useCallback, useMemo } from "react";
-import { ChildArea } from "./ChildArea";
+// import { useState, useCallback, useMemo } from "react";
+// import { ChildArea } from "./ChildArea";
+import { Router } from "./router/Router";
 
 export default function App() {
   // const [count, setCount] = useState(0);
-  const [text, setText] = useState("");
-  const [open, setOpen] = useState(false);
+  // const [text, setText] = useState("");
+  // const [open, setOpen] = useState(false);
 
-  const onChangeText = (e) => {
-    setText(e.target.value);
-  };
-  const onClickOpen = () => {
-    setOpen(!open);
-  };
-  const onClickClose = useCallback(() => setOpen(false), []);
+  // const onChangeText = (e) => {
+  //   setText(e.target.value);
+  // };
+  // const onClickOpen = () => {
+  //   setOpen(!open);
+  // };
+  // const onClickClose = useCallback(() => setOpen(false), []);
 
-  const temp = useMemo(() => 1 + 3, []);
-  console.log(temp);
+  // const temp = useMemo(() => 1 + 3, []);
+  // console.log(temp);
 
   return (
-    <div className="App">
-      <input value={text} onChange={onChangeText} />
-      <br />
-      <br />
-      <button onClick={onClickOpen}>表示</button>
-      <ChildArea open={open} onClickClose={onClickClose} />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Link to="/">Home</Link>
+        <br />
+        <Link to="/page1">Page1</Link>
+        <br />
+        <Link to="/page2">Page2</Link>
+      </div>
+      <Router />
+    </BrowserRouter>
   );
 }
