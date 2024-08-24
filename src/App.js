1fbad6df0688da88fc3203f6bde415cbb9afc2d8
Ayaka Miyake <ayakamiyake7@gmail.com>
2024-08-24 17:21:56 +0900

    Add components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { getDefaultNormalizer } from "@testing-library/react";
 import { BrowserRouter, Link } from "react-router-dom";
 
 import "./App.css";
+import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
+import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
+import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/orgamisms/user/UserCard";
+import { DefaultLayout } from "./components/templates/DefaultLayout";
+import { HeaderOnly } from "./components/templates/HeaderOnly";
 // import { useState, useCallback, useMemo } from "react";
 // import { ChildArea } from "./ChildArea";
 import { Router } from "./router/Router";
+
+const user = {
+  name: "jakee",
+  image:
+    "https://images.unsplash.com/photo-1525253086316-d0c936c814f8?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  email: "hello@gmail.com",
+  phone: "090-0000-0000",
+  company: {
+    name: "テスト株式会社",
+  },
+  website: "https://aaa.com",
+};
 
 export default function App() {
   // const [count, setCount] = useState(0);
@@ -23,13 +42,18 @@ export default function App() {
 
   return (
     <BrowserRouter>
-      <div className="App">
-        <Link to="/">Home</Link>
+      <DefaultLayout>
+        {/* <Link to="/">Home</Link>
         <br />
         <Link to="/page1">Page1</Link>
         <br />
-        <Link to="/page2">Page2</Link>
-      </div>
+        <Link to="/page2">Page2</Link> */}
+        <PrimaryButton>Test</PrimaryButton>
+        <SecondaryButton>Test</SecondaryButton>
+        <br />
+        <SearchInput />
+        <UserCard user={user} />
+      </DefaultLayout>
       <Router />
     </BrowserRouter>
   );
